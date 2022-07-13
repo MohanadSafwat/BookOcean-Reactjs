adc7670cd45998f1b825832ee7be260e285652c3
Mohanad Safwat <57914175+MohanadSafwat@users.noreply.github.com>
2022-07-13 22:48:27 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import Add from "./components/add/add";
 
 function App() {
   return (
-    <Router>
+    <Router basename={process.env.PUBLIC_URL}>
       <div className="App">
         <Navbar />
         <Routes>
